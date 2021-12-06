00500cbcaba06bb884ae13ad6e0be63e5cc68528
AntwanSherif <antwansherif@gmail.com>
2021-12-06 15:59:06 +0200

    :sparkles: Query list of users

--- index.js
+++ index.js
@@ -1,32 +1,62 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
-import { buildSchema, GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql' ;
+import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLNonNull, GraphQLList } from 'graphql';
+
+export const USERS = [
+  {
+    id: 1,
+    name: 'Antwan',
+    email: 'antwansherif@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Hassan',
+    email: 'hassan@gmail.com'
+  }
+];
+
+export const UserType = new GraphQLObjectType({
+  name: 'User',
+  description: 'This represents a user',
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString }
+  })
+});
 
 const RootQueryType = new GraphQLObjectType({
-    name: 'Query',
-    description: 'This is the root query',
-    fields: () => ({
-        hello: {
-            type: GraphQLString,
-            description: 'Just saying hello',
-            resolve: () => 'Hello world!'
-        }
-    })
+  name: 'Query',
+  description: 'This is the root query',
+  fields: () => ({
+    hello: {
+      type: GraphQLString,
+      description: 'Just saying hello',
+      resolve: () => 'Hello world!'
+    },
+    getUsers: {
+      type: new GraphQLList(UserType),
+      description: 'List all users',
+      resolve: () => USERS
+    }
+  })
 });
 
 const schema = new GraphQLSchema({
-    query: RootQueryType
+  query: RootQueryType
 });
-
 
 // Initialize express app
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  '/graphql',
+  graphqlHTTP({
     schema,
     graphiql: true
-}));
+  })
+);
 
 app.listen(4001, () => {
-    console.log(`Server is running on localhost:4001/graphql`);
+  console.log(`Server is running on localhost:4001/graphql`);
 });
